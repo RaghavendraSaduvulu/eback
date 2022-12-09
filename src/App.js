44db29e34e4a5ba3efb6fe1a04d156ce09f8966f
RaghavendraSaduvulu <saduvuluraghavendra@gmail.com>
2022-12-09 09:42:59 +0000

    changes made ebank

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css'
 import Login from './components/Login'
 import Home from './components/Home'
 import NotFound from './components/NotFound'
-import ProtectedRoute from './components/ProtectedRoute'
+// import ProtectedRoute from './components/ProtectedRoute'
 
 // Replace your code here
 const App = () => (
   <Switch>
     <Route exact path="/ebank/login" component={Login} />
-    <ProtectedRoute exact path="/" component={Home} />
+    <Route exact path="/" component={Home} />
     <Route exact path="/bad-path" component={NotFound} />
     <Redirect to="/bad-path" />
   </Switch>
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,36 @@
+import Cookies from 'js-cookie'
+
 import './index.css'
 
-const Home = () => (
-  <div>
-    <h1>Home</h1>
-  </div>
-)
+const Home = props => {
+  const onClickLogout = () => {
+    Cookies.remove('jwt_token')
+    const {history} = props
+    history.replace('/ebank/login')
+  }
+
+  return (
+    <div className="home-container">
+      <div className="header-container">
+        <img
+          className="logo"
+          src="https://assets.ccbp.in/frontend/react-js/ebank-logo-img.png"
+          alt="website logo"
+        />
+        <button className="logout-btn" type="button" onClick={onClickLogout}>
+          logout
+        </button>
+      </div>
+      <div className="home-content">
+        <h1 className="home-heading">Your Flexibility, our Excellence</h1>
+        <img
+          className="digital-card"
+          src="https://assets.ccbp.in/frontend/react-js/ebank-digital-card-img.png"
+          alt="digital card"
+        />
+      </div>
+    </div>
+  )
+}
 
 export default Home
